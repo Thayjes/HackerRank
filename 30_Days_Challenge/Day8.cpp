d7a400149aa371618459006a243adb6251eb8655
Thayjes <thayjes.srivas@gmail.com>
2017-09-15 11:31:04 -0700

    Added details

--- 30_Days_Challenge/Day8.cpp
+++ 30_Days_Challenge/Day8.cpp
@@ -5,7 +5,38 @@
 #include <algorithm>
 #include <map>
 using namespace std;
+//Dictionaries and Maps
+/*Input Format
 
+The first line contains an integer, , denoting the number of entries in the phone book. 
+Each of the  subsequent lines describes an entry in the form of  space-separated values on a single line. The first value is a friend's name, and the second value is an -digit phone number.
+
+After the  lines of phone book entries, there are an unknown number of lines of queries. Each line (query) contains a  to look up, and you must continue reading lines until there is no more input.
+
+Note: Names consist of lowercase English alphabetic letters and are first names only.
+
+Constraints
+
+Output Format
+
+On a new line for each query, print Not found if the name has no corresponding entry in the phone book; otherwise, print the full  and  in the format name=phoneNumber.
+
+Sample Input
+
+3
+sam 99912222
+tom 11122222
+harry 12299933
+sam
+edward
+harry
+Sample Output
+
+sam=99912222
+Not found
+harry=12299933
+
+*/
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
