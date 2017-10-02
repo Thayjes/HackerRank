81462232fa3a0ace4673de5308ee872dc3e2e25e
Thayjes <thayjes.srivas@gmail.com>
2017-10-02 10:09:53 -0700

    Level order traversal

---
+++ 30_Days_Challenge/Day23(BreadthWiseBST).cpp
@@ -0,0 +1,39 @@
+/* Level Order Traversal (Breadth Wise) of Binary Search Tree
+Sample Input
+
+6
+3
+5
+4
+7
+2
+1
+Sample Output
+
+3 2 5 1 4 7 
+
+*/
+#include <iostream>
+#include <cstddef>
+#include <queue>
+#include <string>
+#include <cstdlib>
+
+void levelOrder(Node * root){
+      //Write your code here
+        Node *temp;
+        queue<Node*> q;   
+        temp = root;
+        while(temp){
+            cout << temp->data << " ";
+            if(temp->left){
+                q.push(temp->left);
+            }
+            if(temp->right){
+                q.push(temp->right);
+            }
+            temp = q.front();
+            q.pop();
+        }
+        
+	}
